9dc0b465c06ec7e76e0019a16ec18f25d293249c
Markus Hager <meks77@gmail.com>
2020-04-24 20:29:00 +0200

    Maven structure for Quarkus added

--- co-backend/src/test/java/at/itsv/sogo/euvsvirus/coronaoverflow/NativeCatRessourceTest.java
+++ co-backend/src/test/java/at/itsv/sogo/euvsvirus/coronaoverflow/NativeCatRessourceTest.java
@@ -3,7 +3,7 @@ package at.itsv.sogo.euvsvirus.coronaoverflow;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeWhateverServiceIT extends CatRessourceTest {
+public class NativeCatRessourceTest extends CatRessourceTest {
 
     // Execute the same tests but in native mode.
 }

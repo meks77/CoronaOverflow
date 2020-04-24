2d787363c756c639604e66a0dccf47245277951d
Markus Hager <meks77@gmail.com>
2020-04-24 20:02:07 +0200

    Maven structure for Quarkus added

---
+++ co-backend/src/test/java/org/euvsvirus/at/coronaflow/NativeWhateverServiceIT.java
@@ -0,0 +1,9 @@
+package org.euvsvirus.at.coronaflow;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeWhateverServiceIT extends WhateverServiceTest {
+
+    // Execute the same tests but in native mode.
+}

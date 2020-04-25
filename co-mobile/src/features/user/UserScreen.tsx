0ed4ae207215c1d073fb04b5bc6a29a683889e1f
Milan Vojinovic <milan.vojinovic@itsv.at>
2020-04-25 18:25:17 +0200

    co-mobile: - added user screen

--- co-mobile/src/features/user/UserScreen.tsx
+++ co-mobile/src/features/user/UserScreen.tsx
@@ -1,13 +1,74 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
-import {Text} from 'react-native-elements';
+import {AsyncStorage, ScrollView, View} from 'react-native';
+import {Button, Icon, Input, ListItem, Text} from 'react-native-elements';
 import {colors} from '../../styles/Colors';
+import Moment from "moment";
+import {UserDetailListItem} from "./components/UserDetailListItem";
+import {Constants} from "../../services/Constants";
 
 export class UserScreen extends React.Component {
+
+    state = {
+        userID: "",
+        newUserID: ""
+    }
+
+    componentDidMount = () => {
+        AsyncStorage.getItem(Constants.KEY_USERID)
+            .then((userID) => {
+                this.setState({userID: userID})
+            })
+    }
+
+    onSaveUsername = (userID: string) => {
+        try {
+            AsyncStorage.setItem(Constants.KEY_USERID, userID)
+                .then(() => {
+                    AsyncStorage.getItem(Constants.KEY_USERID)
+                        .then((userID) => {
+                            this.setState({userID: userID})
+                        });
+                });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     render() {
         return (
             <ScrollView style={{flex: 1, backgroundColor: colors.light_grey}}>
-                <Text>UserScreen</Text>
+                <View style={{alignItems: "center", marginTop: 50, marginBottom: 10}}>
+                    <Icon
+                        name="user"
+                        type="evilicon"
+                        color={colors.primary_light_green}
+                        size={150}
+                    />
+                    <Text style={{fontSize: 30, color: colors.dark_grey, marginTop: 10, marginBottom: 40}}>
+                        {this.state.userID ? this.state.userID : "NO_USERID"}
+                    </Text>
+                    <UserDetailListItem title={"Name"} subtitle={"Max Mustermann"}/>
+                    <UserDetailListItem title={"Email"} subtitle={"max.mustermann@example.com"}/>
+
+                    <View style={{width: "100%", backgroundColor: colors.primary_white, paddingHorizontal: 5, paddingVertical:20, marginTop: 30, marginBottom: 50}}>
+                    <Input
+                        containerStyle={{}}
+                        label={"Enter new username"}
+                        labelStyle={{color: colors.dark_grey}}
+                        placeholder="Example: max.mustermann"
+                        value={this.state.newUserID}
+                        onChangeText={(value) => {
+                            console.log(value);
+                            this.setState({newUserID: value});
+                        }}
+                    />
+                    <Button
+                        title={"Save Username"}
+                        titleStyle={{color: colors.primary_light_green, textAlign:"center", justifyContent: "center"}}
+                        buttonStyle={{marginHorizontal: 50, marginTop: 30, backgroundColor: colors.primary_white, borderWidth: 2, borderColor: colors.primary_light_green, borderRadius: 500, alignItems: "center", paddingHorizontal:50, justifyContent: "center"}}
+                        onPress={() => {this.onSaveUsername(this.state.newUserID)}} />
+                    </View>
+                </View>
             </ScrollView>
         );
     }
---
+++ co-mobile/src/features/user/components/UserDetailListItem.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {ListItem} from 'react-native-elements';
+import {colors} from "../../../styles/Colors";
+
+interface Props {
+    title: string;
+    subtitle: string;
+}
+
+export class UserDetailListItem extends React.Component<Props> {
+    render() {
+        return (
+            <ListItem
+                title={this.props.title}
+                containerStyle={{
+                    backgroundColor: colors.primary_white,
+                    width: '100%'
+                }}
+                titleStyle={{color: colors.dark_grey, fontSize: 14, marginBottom: 3}}
+                subtitleStyle={{color: colors.dark_grey, fontSize: 17}}
+                subtitle={this.props.subtitle}
+                bottomDivider
+            />
+        );
+    }
+}

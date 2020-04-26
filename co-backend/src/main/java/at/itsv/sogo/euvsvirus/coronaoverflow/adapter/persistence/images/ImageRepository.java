290ac0c7abd013824b5f72df0004ddef0b8c00f3
Martin Soelkner <martin.soelkner@itsv.at>
2020-04-26 14:26:12 +0200

    image ressource try

---
+++ co-backend/src/main/java/at/itsv/sogo/euvsvirus/coronaoverflow/adapter/persistence/images/ImageRepository.java
@@ -0,0 +1,19 @@
+package at.itsv.sogo.euvsvirus.coronaoverflow.adapter.persistence.images;
+
+import org.apache.commons.io.IOUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.IOException;
+
+@ApplicationScoped
+public class ImageRepository {
+
+    public byte[] findImageForId(String imageId) {
+        try {
+            return IOUtils.toByteArray(getClass().getResourceAsStream(imageId));
+        } catch (IOException e) {
+            throw new IllegalStateException("error while reading dummy picture", e);
+        }
+    }
+
+}
